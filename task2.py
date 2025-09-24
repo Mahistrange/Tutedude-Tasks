b2726a24b4fa4719d823f48089a347cdc8d052cd
Mahipal Reddy <106643781+Mahistrange@users.noreply.github.com>
2025-09-24 21:49:47 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,7 @@
+#inputs from user
+
+fname = str(input("please enter your first name: "))
+lname = str(input("please enter your last name: "))
+
+#printing the custom greeting
+print ("Hello," + fname + " " + lname + "! Welcome to the python propgram from tutedude.")
